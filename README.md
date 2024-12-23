13afba21484a484b69453d42d3cb56594f61970a
Fábio Figueiroa <45983834+fabioff30@users.noreply.github.com>
2024-12-23 20:28:39 +0000

    codigo formatado com o prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-clone
+
 Clone do Tabnews do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
-import { useState } from 'react';
-import styles from './styles.module.css';
+import { useState } from "react";
+import styles from "./styles.module.css";
 
 function Home() {
-    const [message, setMessage] = useState("Clique no botão para algo incrível!");
+  const [message, setMessage] = useState("Clique no botão para algo incrível!");
 
-    const handleClick = () => {
-        setMessage("Bem-vindo ao futuro com React e Next.js! 🚀");
-    };
+  const handleClick = () => {
+    setMessage("Bem-vindo ao futuro com React e Next.js! 🚀");
+  };
 
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Explorando o Infinito</h1>
-            <p className={styles.message}>{message}</p>
-            <button className={styles.button} onClick={handleClick}>
-                Transformar
-            </button>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Explorando o Infinito</h1>
+      <p className={styles.message}>{message}</p>
+      <button className={styles.button} onClick={handleClick}>
+        Transformar
+      </button>
+    </div>
+  );
 }
 
 export default Home;
+
+function teste() {
+  console.log("teste123");
+}
